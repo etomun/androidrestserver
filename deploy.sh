560b697b1ecc0f0978e36c303613e61f05a55bb2
etomun <ngadiyantoeto@gmail.com>
2023-12-27 14:22:39 +0700

    add ddns config

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,12 @@ echo "Starting deployment process..."
 # Pull the latest changes from the GitHub
 git pull origin master
 
+# Set domain name using dnsmasq
+dnsmasq -C ~/.local-dns.conf
+
 # Install or update Python dependencies
 pip install -r requirements.txt
-echo "All requirements is set"
+echo "All requirements have been set"
 
 # Restart the FastAPI application using uvicorn
 uvicorn main:app --host 0.0.0.0 --port 1717 --reload
